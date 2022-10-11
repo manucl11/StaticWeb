aab1b0faab76d58491538ba743a73db69e939417
manucl12 <91848502+manucl12@users.noreply.github.com>
2022-10-11 13:08:44 +0200

    Actualización 2 primeras horas

---
+++ main.js
@@ -0,0 +1,45 @@
+
+var participants = [];
+
+const showParticipants = (data) => {
+    let arrayAuxiliar = [];
+    data.forEach(element => {
+        arrayAuxiliar.push(element.name);
+    });
+
+    //$("#users").text("Participants: " + arrayAuxiliar);
+    $("#users").html("<strong>Participants: </strong>" + arrayAuxiliar);
+}
+
+const getWinner = (data) => {
+
+    const aleat = Math.floor(Math.random() * data.length);
+
+    $("#selected").html("<strong>Seleccionado: </strong>" + data[aleat].name);
+}
+
+$(document).ready(function(){
+
+    //jsonplaceholder.typicode.com/users
+
+    $.ajax({
+
+        type: "GET",
+        url: "https://jsonplaceholder.typicode.com/users",
+        contentType: "application/json",
+        success: function(response){
+
+            participants = response;
+            showParticipants(participants);
+        },
+        error: function(error){
+            console.log(error);
+            alert(error);
+        }
+    });
+
+    $(".btn").click(function(){
+
+        getWinner(participants);
+    });
+});
