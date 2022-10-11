5a4852c176054d3efeeb9513bb75bd4c447ad900
manucl12 <91848502+manucl12@users.noreply.github.com>
2022-10-11 13:54:15 +0200

    actualizacion de prueba 3

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 
-var participants = ["guapa", "buena gente", "a veces un poco tona", "esta buena", "graciosa"];
+var participants = ["guapa", "buena gente", "a veces un poco tonta", "esta buena", "graciosa"];
 
 const showParticipants = (data) => {
     let arrayAuxiliar = [];
-    /*data.forEach(element => {
+    data.forEach(element => {
         arrayAuxiliar.push(element.name);
-    });*/
+    });
 
     //$("#users").text("Participants: " + arrayAuxiliar);
-    $("#users").html("<strong>Participants: </strong>" + participants);
+    $("#users").html("<strong>Participants: </strong>" + arrayAuxiliar);
 }
 
 const getWinner = (data) => {
 
     const aleat = Math.floor(Math.random() * data.length);
 
-    $("#selected").html("<strong>Seleccionado: </strong>" + data[aleat].name);
+    $("#selected").html("<strong>Seleccionado: </strong>" + data[aleat]);
 }
 
 $(document).ready(function(){
